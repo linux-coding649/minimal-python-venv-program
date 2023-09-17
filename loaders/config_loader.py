54111ae8bac363aa019c3e3eb09c78aa113eda51
linux-coding649 <104065079+linux-coding649@users.noreply.github.com>
2023-09-17 19:58:18 +1200

    Update config_loader.py

--- loaders/config_loader.py
+++ loaders/config_loader.py
@@ -1,9 +1,17 @@
+# Created by linux649 under the GNU GPLv3 License
+
+# VEP (Virtual Environment Program) Configuration Loader Copyright (C) 2023  linux649
+# This program comes with ABSOLUTELY NO WARRANTY; for details go to <https://www.gnu.org/licenses>'.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; go to <https://www.gnu.org/licenses> for details.
+
 import configparser
 
 def createConfig(cfile):
     cfg = configparser.ConfigParser()
     cfg['External Commands'] = {'registry-json':'registry.external.json',
-                                'library-dir': 'loaders.extCmds'}
+                                'library-dir': 'extCmds',
+                                'compressed-dir':'cmpCmds'}
     with open(cfile, 'w') as cfgf:
         cfg.write(cfgf)
 
@@ -12,16 +20,17 @@ def readConfig(cfile):
     cfg.read(cfile)
     if 'External Commands' not in cfg.sections():
         print('Invalid Configuration')
-        return False, False
+        return False, False, False
     try:
         regf = cfg['External Commands']['registry-json']
         rloca = cfg['External Commands']['library-dir']
+        cloca = cfg['External Commands']['compressed-dir']
     except KeyError:
         print('Invalid Configuration')
-        return False, False
-    if not regf or not rloca:
+        return False, False, False
+    if not regf or not rloca or not cloca:
         print('Invalid Configuration')
-        return False, False
-    return regf, rloca
+        return False, False, False
+    return regf, rloca, cloca
 
 
